b5e090cf060f92d45ecc2a02a931d460b11742fd
t.aponte <t.aponte@uniandes.edu.co>
2025-02-10 19:39:49 -0500

    array_final

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -25,12 +25,14 @@ def is_present(my_list, element, cmp_function):
     return -1
 
 def add_first(my_list, element):
-    my_list["elements"] = element
+    my_list["elements"].insert(0, element)
     my_list["size"] += 1
+    return my_list
     
 def add_last(my_list, element):
     my_list["elements"].append(element)
     my_list["size"] += 1
+    return my_list
 
 def size(my_list):
     return my_list["size"]
@@ -49,10 +51,39 @@ def last_element(my_list):
     return my_list["elements"][-1]
 
 def remove_first(my_list):
-    return my_list["elements"].pop(0)
+    my_list["elements"].pop(0)
+    my_list["size"] -= 1
+    return my_list
 
 def remove_last(my_list):
-    return my_list["elements"].pop()
+    my_list["elements"].pop()
+    my_list["size"] += 1
+    return my_list
 
 def insert_element(my_list, element, pos):
     my_list["elements"].insert(pos,element)
+    my_list["size"] += 1
+    return my_list
+
+def delete_element(my_list, element):
+    my_list["elements"].pop(element)
+    my_list["size"] -= 1
+    return my_list
+
+def change_info(my_list, pos, new_info):
+    my_list["elements"][pos] = new_info
+    return my_list
+
+def exchange(my_list, pos_1, pos_2):
+    pos1 = my_list["elements"][pos_1]
+    pos2 = my_list["elements"][pos_2]
+    my_list["elements"][pos_1] = pos2
+    my_list["elements"][pos_2] = pos1
+    return my_list
+    
+def sub_list(my_list, pos_i, num_elements):
+    sub_list = new_list()
+    sub_list["size"] = num_elements
+    for i in range(pos_i, pos_i+num_elements):
+        sub_list["elements"].append(my_list["elements"][i])
+    return sub_list
